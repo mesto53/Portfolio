d3f4a875042b350a085c892a82787c3808ec2b06
mesto <mhmdmestrah53@gmail.com>
2023-09-19 20:42:36 +0300

    update nav

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -5,9 +5,9 @@ import { NavLink  } from 'react-router-dom';
 export default function Nav() {
     return (
         <nav className=''>
-            <NavLink className='text-white font-[600] mr-3' to="/">About</NavLink>
-            <NavLink className='text-white font-[600] mr-3' to="/services">Services</NavLink>
-            <NavLink className='text-white font-[600] mr-3' to="/portfolio">Portfolio</NavLink>
+            <NavLink className='text-white font-[600]' to="/">About</NavLink>
+            <NavLink className='text-white font-[600]' to="/services">Services</NavLink>
+            <NavLink className='text-white font-[600]' to="/portfolio">Portfolio</NavLink>
             <NavLink className='text-white font-[600]' to="/contact">Contact</NavLink>
         </nav>
     )
